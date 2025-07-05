ee2dfeb77acfad4264dc143b520ffdc8da39a272
Akshay Poudel <akshaypoudel66@gmail.com>
2025-07-05 21:28:46 +0530

    Added New Navbar

--- lib/home/bottom_bar.dart
+++ lib/home/bottom_bar.dart
@@ -1,8 +1,6 @@
 import 'package:crystal_navigation_bar/crystal_navigation_bar.dart';
 import 'package:flutter/material.dart';
-//import 'package:get/get.dart';
 import 'package:trunriproject/profile/profileScreen.dart';
-//import 'addAccommodationScreen.dart';
 import 'explorScreen.dart';
 import 'favoriteRestaurantsScreen.dart';
 import 'home_screen.dart';
@@ -22,78 +20,6 @@ class _MyBottomNavBarState extends State<MyBottomNavBar> {
     const ExplorScreen(),
     const ProfileScreen(),
   ];
-
-  Widget _buildOption({
-    required IconData icon,
-    required String label,
-    required VoidCallback onTap,
-  }) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Column(
-        children: [
-          CircleAvatar(
-            radius: 30,
-            backgroundColor: Colors.redAccent,
-            child: Icon(icon, color: Colors.white, size: 30),
-          ),
-          const SizedBox(height: 10),
-          Text(label, style: const TextStyle(fontSize: 10)),
-        ],
-      ),
-    );
-  }
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   return Scaffold(
-  //     backgroundColor: Colors.transparent,
-  //     extendBody: true,
-  //     bottomNavigationBar: Padding(
-  //       padding: const EdgeInsets.only(bottom: 10),
-  //       child: Container(
-  //         margin: const EdgeInsets.only(left: 20, right: 20, bottom: 0),
-  //         decoration: BoxDecoration(
-  //           boxShadow: [
-  //             BoxShadow(
-  //               color: Colors.black.withOpacity(0.5),
-  //               blurRadius: 25,
-  //               offset: const Offset(8, 20),
-  //             )
-  //           ],
-  //         ),
-  //         child: ClipRRect(
-  //           borderRadius: BorderRadius.circular(30),
-  //           child: SizedBox(
-  //             height: 60,
-  //             child: BottomNavigationBar(
-  //               backgroundColor: Colors.transparent,
-  //               selectedItemColor: Colors.redAccent,
-  //               unselectedItemColor: Colors.black,
-  //               currentIndex: myCurrentIndex,
-  //               onTap: (index) {
-  //                 setState(() {
-  //                   myCurrentIndex = index;
-  //                 });
-  //               },
-  //               items: const [
-  //                 BottomNavigationBarItem(
-  //                     icon: Icon(Icons.home), label: "Home"),
-  //                 BottomNavigationBarItem(
-  //                     icon: Icon(Icons.favorite), label: "Favorite"),
-  //                 BottomNavigationBarItem(
-  //                     icon: Icon(Icons.explore), label: "Discover"),
-  //                 BottomNavigationBarItem(
-  //                     icon: Icon(Icons.person_outline), label: "Profile"),
-  //               ],
-  //             ),
-  //           ),
-  //         ),
-  //       ),
-  //     ),
-  //     body: pages[myCurrentIndex],
-  //   );
-  // }
 
   @override
   Widget build(BuildContext context) {
@@ -124,35 +50,6 @@ class _MyBottomNavBarState extends State<MyBottomNavBar> {
           CrystalNavigationBarItem(icon: Icons.person_2_rounded),
         ],
       ),
-
-      // ClipRRect(
-      //   borderRadius: BorderRadius.circular(30),
-      //   child: SizedBox(
-      //     height: 60,
-      //     child: BottomNavigationBar(
-      //       backgroundColor: Colors.transparent,
-      //       selectedItemColor: Colors.redAccent,
-      //       unselectedItemColor: Colors.black,
-      //       currentIndex: myCurrentIndex,
-      //       onTap: (index) {
-      //         setState(() {
-      //           myCurrentIndex = index;
-      //         });
-      //       },
-      //       items: const [
-      //         BottomNavigationBarItem(
-      //             icon: Icon(Icons.home), label: "Home"),
-      //         BottomNavigationBarItem(
-      //             icon: Icon(Icons.favorite), label: "Favorite"),
-      //         BottomNavigationBarItem(
-      //             icon: Icon(Icons.explore), label: "Discover"),
-      //         BottomNavigationBarItem(
-      //             icon: Icon(Icons.person_outline), label: "Profile"),
-      //       ],
-      //     ),
-      //   ),
-      // ),
-
       body: pages[myCurrentIndex],
     );
   }
