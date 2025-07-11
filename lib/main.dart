f5ce26795eb323cb2a0b15b0d8f95ddc92a725f9
Akshay Poudel <akshaypoudel66@gmail.com>
2025-07-11 15:07:07 +0530

    chats module working

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,14 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:trunriproject/SplashScreen.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await GoogleFonts.pendingFonts([
+    GoogleFonts.caveat().fontFamily,
+  ]);
   runApp(const MyApp());
 }
 
